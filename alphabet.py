c89334772ed5e9d23398bb94862db74da895244b
khadada <lkhadada@gmail.com>
2021-12-02 23:06:47 +0100

    I do some modifies on the code

--- alphabet.py
+++ alphabet.py
@@ -7,12 +7,15 @@ class Alphabet:
 
     def lower_case_letters(self):
         self.lower_letters_list.extend([chr(i) for i in range(ord("a"), ord("z"))])
+        return self.lower_letters_list
 
     def upper_case_letters(self):
         self.upper_letters_list.extend([chr(i) for i in range(ord("A"), ord("Z"))])
+        return self.upper_letters_list
 
-    def numbers(self):
-        self.number_list.extend([i for i in range(0,9)])
+    def numbers(self,start=0,end=10):
+        self.number_list.extend([i for i in range(start,end)])
+        return self.number_list
 
     def chars_nums(self):
         self.lower_case_letters()
@@ -21,3 +24,4 @@ class Alphabet:
         self.all_characters.extend(self.lower_letters_list)
         self.all_characters.extend(self.upper_letters_list)
         self.all_characters.extend(self.number_list)
+        return self.all_characters
